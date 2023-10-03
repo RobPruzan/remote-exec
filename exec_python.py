404e27b9c63b4dae9a75013b3d111bf3196596ab
RobPruzan <rapruzan@buffalo.edu>
2023-10-03 02:07:16 -0400

    update to support more metadata

---
+++ exec_python.py
@@ -0,0 +1,55 @@
+import os, json
+
+from dataclasses import dataclass, asdict
+from typing import List, Dict
+
+# whats concatenated:
+# @dataclass
+# class Node:
+#     ID: string
+#     value: int
+
+
+# def algorithm(adjList: Dict[Node, Node], start_node: Node):
+#     # your code here
+#     pass
+
+# def apply_dict(step):
+#     if (type(step) == list):
+#         if len(step) > 0 and type(step[0]) == list:
+#             return [s.as]
+
+
+@dataclass(
+    eq=True,
+    frozen=True,
+)
+class Node:
+    ID: str
+    value: int
+
+    # def __json__(self):
+    #     dct = {"ID": self.ID, "value": self.value}
+    #     return json.dumps(dct)
+
+
+class ExtendedEncoder(json.JSONEncoder):
+    def default(self, obj):
+        # if hasattr(obj, 'dictify'):
+        #     return obj.dictify()
+        # return super
+        return asdict(obj)
+
+
+_node = lambda node: Node(ID=node.split(",")[0], value=int(node.split(",")[1]))
+
+
+adjacencyList = {
+    _node(node): [Node(n, v) for n, v in neighbors]
+    for node, neighbors in json.loads(os.getenv("ADJACENCY_LIST", "[]")).items()
+}
+
+start_node = Node(
+    ID=json.loads(os.getenv("START_NODE")),
+    value=json.loads(os.getenv("START_NODE_VALUE")),
+)
